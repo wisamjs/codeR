408880bf3616d78e97e4310c89936894fc0ba93e
wzaghal <wisamzaghal@gmail.com>
2015-06-25 09:07:04 -0700

    initial view setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 angular
   .module('app', ['ionic'])
   .config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
+  $urlRouterProvider.otherwise('/app/home')
 
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl:'sections/home/home.html'
-  })
-})
+//   $stateProvider
+//     .state('home', {
+//       url: '/',
+//       templateUrl:'sections/home/home.html'
+//     })
+//     .state('candidates', {
+//       url: '/candidates',
+//       templateUrl: 'sections/candidates/candidates.html'
+//     })
+// })
+
+  $stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "sections/main.html"
+    })
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "sections/home/home.html",
+        }
+      }
+    })
+        .state('app.candidates', {
+      url: "/candidates",
+      views: {
+        'home-tab': {
+          templateUrl: "sections/candidates/candidates.html"
+        }
+      }
+    })
+  });
