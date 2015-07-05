df7bb9a58c28818dd839bd40cf3e5379715df900
wzaghal <wisamzaghal@gmail.com>
2015-07-04 22:21:21 -0400

    update candidate-service when new api calls are made

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@
 
 import '../scss/fonts.scss';
 import '../bower_components/ionic/scss/ionic.scss';
-
+import R from 'Ramda';
 import mainTemplate from './sections/main.html';
 import candidateTemplate from './sections/candidates/candidates.html';
 import homeTemplate from './sections/home/home.html';
 import {userFormModule, candidateModule} from './components/components';
 import {coreModule} from './core/core';
+
+angular.module('3rdParty', [])
+.factory('R',function(){
+  return R;
+})
+
+
 angular
   .module('app', [
     'ionic',
+    '3rdParty',
     userFormModule.name,
     coreModule.name,
     candidateModule.name
--- src/components/candidate/candidate-service.js
+++ src/components/candidate/candidate-service.js
@@ -1,8 +1,9 @@
-let candidateService = ($http, $q, localStorageService) => {
+let candidateService = ($http, $q, localStorageService, R) => {
   const API_URL = 'http://localhost:3000/api/people';
   let candidates = [];
   let usedIDs = [];
   let nextID = 0;
+  let skills = [];
 
   let isUsed = (num) => {
     return usedIDs.indexOf(num) !== -1;
@@ -16,13 +17,13 @@ let candidateService = ($http, $q, localStorageService) => {
     usedIDs.length = 0;
   };
 
-  let getChosenSkills = () => {
-    return localStorageService.get('project').labels;
+  let getCachedSkills = () => {
+    skills = localStorageService.get('project').labels;
+    return skills;
 
   }
 
   let saveCandidates = (res) => {
-
     clearUsed();
     candidates = res.data;
     return res;
@@ -30,8 +31,7 @@ let candidateService = ($http, $q, localStorageService) => {
   }
 
   let getAllCandidates = () => {
-      let skills = getChosenSkills();
-      console.log(skills);
+      let skills = getCachedSkills();
       return $http({
         url: API_URL,
         method: 'GET',
@@ -47,16 +47,34 @@ let candidateService = ($http, $q, localStorageService) => {
 
   }
 
+  let newCandidates = () => {
+    return candidates.length === 0;
+
+  }
+
+  let newSkills = () => {
+    let cachedSkills = getCachedSkills();
+    return skills.length === 0 || equalArrs(skills, cachedSkills);
+
+  }
+
+  let equalArrs = (arr1, arr2) => {
+    let sortedArr1 = arr1.sort();
+    let sortedArr2 = arr2.sort();
+
+    return R.equals(sortedArr1, sortedArr2);
+  }
+
   let getCandidate = () => {
-    if (candidates.length > 0){
-      return getNextCandidate();
 
-
-    }else{
-    return getAllCandidates()
+    if ( newCandidates() || newSkills() ){
+      return getAllCandidates()
       .then(saveCandidates)
       .then(getNextCandidate)
       }
+    else{
+      return getNextCandidate();
+    }
   }
 
   return {getCandidate};
